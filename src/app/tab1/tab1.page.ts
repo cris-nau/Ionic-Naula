63418726d276d442e1d9d450723c8f4f2e0af11b
cris-nau <naulasantiago537@gmail.com>
2023-02-13 11:41:40 -0500

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['Style.css']
 })
 export class Tab1Page {
 
-  constructor() {}
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert1() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'Hola',
+      buttons: ['OK', 'Cancel'],
+    });
+
+    await alert.present();
+  }
+  async presentAlert2() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'Hola',
+      buttons: ['OK', 'Cancel'],
+    });
+
+    await alert.present();
+  }
+  async presentAlert3() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'Hola',
+      buttons: ['OK', 'Cancel'],
+    });
+
+    await alert.present();
+  }
 
 }
